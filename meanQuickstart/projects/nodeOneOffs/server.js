4fe3c897d987d45fdf2e3e54d52981b922fee806
Brian R. Smith <brianrsmith080@gmail.com>
2021-09-28 17:31:49 +0000

    removed incomplete angular/projects/tour-of-heroes. complete version stored locally.

--- meanQuickstart/projects/nodeOneOffs/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-console.log('server.js: test')
-http.createServer(function (req, res) {
-			res.writeHead(200, {'Content-Type': 'text/plain'});
-			res.end('Hello World!');
-		}).listen(80); 
-
